333ba0388a069b4057bc06e768b68e4f80c073d9
Josep <pujol.rabat@gmail.com>
2018-11-23 13:58:20 +0000

    Hello Flask, Hello Unitest

--- run.py
+++ run.py
@@ -1,7 +1,27 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 usernames = []
 riddles = {} # including answers
 user_answers = {}
+
+
+@app.route('/')
+def index():
+    """
+    Hello Flask!
+    """
+    return 'Hello flask!'
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP'),
+            port=int(os.getenv('PORT')),
+            debug=True)
---
+++ test_run.py
@@ -0,0 +1,15 @@
+import run
+import unittest
+
+# Hello world test
+class test_run(unittest.TestCase):
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
+
+
+class TestRun(unittest.TestCase):
+    '''
+    Our test suite for fiddle-me-this, run.py file
+    '''
+    
+    
