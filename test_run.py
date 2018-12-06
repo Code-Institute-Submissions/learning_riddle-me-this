dc81060283c9fdf2e14ea436316c9be37867f9d9
Josep <pujol.rabat@gmail.com>
2018-12-06 17:43:52 +0000

    amended add_usernames test to test for unique usernames

--- test_run.py
+++ test_run.py
@@ -2,13 +2,14 @@ import requests
 import run
 import unittest
 
+# TODO
+# TO BE REVIEWED. should we use index function??
+# is it ok to have variables in a test function?
 
 class TestRun(unittest.TestCase):
     """
     Our test suite for riddle-me-this, run.py file
     """
-    # TO BE REVIEWED. should we use index function??
-    # is it ok to have variables in a test function?
     def test_indexhtml_get(self):
         """
         Renders index.html when GET request 
@@ -19,7 +20,6 @@ class TestRun(unittest.TestCase):
         html_text = requests.get(url, headers=headers).text
         self.assertIn(btn_element, html_text)
 
-    # TO BE REVIEWED. should we use index function??
     def test_indexhtml_post(self):
         """
         Returns the username when POST request
@@ -27,29 +27,31 @@ class TestRun(unittest.TestCase):
         url = 'https://riddle-me-this-joseppujol.c9users.io/'
         username = 'theUser'
         headers = {'Connection':'close'}
+        
         r = requests.get(url + username, headers=headers)
         html_text = r.text
         self.assertIn(username, html_text)
     
-    
-    def test_add_usernames(self):
+    def test_add_usernames_only_unique(self):
         """
         Usernames added correctly in temporary variable run.usernames
         """
         url = 'https://riddle-me-this-joseppujol.c9users.io/'
-        usernames_test = ['username1', 'username2',]
+        usrnames_test = ['username1', 'username2', 'username3', 'username1']
+        unique_usrnames = ['username1', 'username2', 'username3',]
         headers = {'user-agent': 'Headless', 
                    'origin': 'http://riddle-me-this-joseppujol.c9users.io',
                    'Connection':'close'}
         cookies = {'c9.live.user.click-through': 'ok'}
         
-        for user in usernames_test:
+        for user in usrnames_test:
             data = {'username': user}
             r = requests.post(url, data=data,
                               headers=headers, 
                               cookies=cookies)
         html_text = requests.get(url + 'print_usernames', 
                                  headers=headers).text
-        usernames_html = [item for item in html_text.split(' ') if item != '']
-        self.assertEqual(usernames_html, usernames_test)
-        
+        usrnames_html = [item for item in html_text.split(' ') if item != '']
+        self.assertEqual(usrnames_html, unique_usrnames)
+    
+    
