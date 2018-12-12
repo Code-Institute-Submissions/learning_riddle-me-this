5f4c7f1d32f7df4b5918ccaa5e5cc77110c934a0
Josep <pujol.rabat@gmail.com>
2018-12-12 19:32:20 +0000

    added one riddle to the render_riddle function

--- run.py
+++ run.py
@@ -11,17 +11,25 @@ app = Flask(__name__)
 usernames = []
 user_answers = {}
 riddles = []
+answered_riddles = []
+
 
 def read_riddlesjson():
     with open('./data/riddles.json', 'r', encoding='utf-8') as f:
         riddles = json.load(f)
     return riddles
+riddles = read_riddlesjson()
 
 
 def next_riddle(riddles):
     if riddles:
         riddle = riddles.pop(0)
     return riddle, riddle['riddle_id']
+
+
+def store_answered_riddles(riddle_id, answer):
+    answered_riddles.append({'riddle_id': riddle_id, 'answer': answer})
+    return answered_riddles
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -52,10 +60,20 @@ def print_users():
     return all_users
 
 
-@app.route('/riddle/<username>')
+@app.route('/riddle/<username>', methods=['GET', 'POST'])
 def render_riddle(username):
+    """
+    Display a riddle and a text box to answer it.
+    """
+    riddle_text = riddles[1]['riddle']
     
-    return render_template('riddle.html', username=username)
+    if request.method == 'POST':
+        answer = request.form['answer'].lower()
+        right_answer = riddles[1]['answer'].lower()
+        is_correct = (True if answer == right_answer else False)
+        return (str(is_correct) , answer)
+        
+    return render_template('riddle.html', riddle_text=riddle_text)
 
 
 if __name__ == '__main__':
